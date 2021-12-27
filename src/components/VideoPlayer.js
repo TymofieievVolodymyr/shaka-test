9d50e7bcae593b7665cbeb2457b1803fc987852f
Volodymyr Timofieiev <vovanium.timofeev@gmail.com>
2021-12-27 22:34:43 +0200

    test

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -3,7 +3,7 @@ import ShakaPlayer from 'shaka-player-react';
 import 'shaka-player/dist/controls.css';
 import mpd from '../manifest/dash.mpd'
 import playlist from '../hls/playlist.m3u8';
-import play from '../hlssplitfolders/playlist.m3u8'
+
 
 function VideoPlayer() {
   const ref = React.useRef(null);
@@ -16,7 +16,6 @@ function VideoPlayer() {
     <div>
       <ShakaPlayer ref={ref} src={mpd}/>
       {/*<ShakaPlayer ref={ref} src={playlist}/>*/}
-      {/*<ShakaPlayer ref={ref} src={play}/>*/}
       {/*<ShakaPlayer ref={ref} src='https://5b44cf20b0388.streamlock.net:8443/vod/smil:hls-maudios-prod.smil/playlist.m3u8'/>*/}
     </div>
   );
