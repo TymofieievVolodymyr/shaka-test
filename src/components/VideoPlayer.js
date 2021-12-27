b39ba1229f69c90bd632e0f16370be73ab81eb38
Volodymyr Timofieiev <vovanium.timofeev@gmail.com>
2021-12-27 22:14:17 +0200

    added shaka and react wrapper

---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ShakaPlayer from 'shaka-player-react';
+import 'shaka-player/dist/controls.css';
+import mpd from '../manifest/dash.mpd'
+import playlist from '../hls/playlist.m3u8';
+import play from '../hlssplitfolders/playlist.m3u8'
+
+function VideoPlayer() {
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+    window.getShakaInst = () => ref.current;
+  }, []);
+
+  return (
+    <div>
+      <ShakaPlayer ref={ref} src={mpd}/>
+      {/*<ShakaPlayer ref={ref} src={playlist}/>*/}
+      {/*<ShakaPlayer ref={ref} src={play}/>*/}
+      {/*<ShakaPlayer ref={ref} src='https://5b44cf20b0388.streamlock.net:8443/vod/smil:hls-maudios-prod.smil/playlist.m3u8'/>*/}
+    </div>
+  );
+}
+
+export default VideoPlayer;
+
