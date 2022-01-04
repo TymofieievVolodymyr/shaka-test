630ff96b401c438ba518c5fadf6dba62ed0dfe4b
Volodymyr Timofieiev <vovanium.timofeev@gmail.com>
2022-01-04 18:33:44 +0200

    test

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -3,6 +3,7 @@ import ShakaPlayer from 'shaka-player-react';
 import 'shaka-player/dist/controls.css';
 import mpd from '../manifest/dash.mpd'
 import playlist from '../hls/playlist.m3u8';
+import playlisttest from '../hlsall/playlist.m3u8'
 
 
 function VideoPlayer() {
@@ -10,13 +11,16 @@ function VideoPlayer() {
 
   React.useEffect(() => {
     window.getShakaInst = () => ref.current;
+    console.log(ref.current.player);
   }, []);
 
   return (
     <div>
       <ShakaPlayer ref={ref} src={mpd}/>
-      {/*<ShakaPlayer ref={ref} src={playlist}/>*/}
+      {/*<ShakaPlayer ref={ref} src='http://localhost:2015/playlist.m3u8'/>*/}
+      {/*<ShakaPlayer ref={ref} src={playlisttest}/>*/}
       {/*<ShakaPlayer ref={ref} src='https://5b44cf20b0388.streamlock.net:8443/vod/smil:hls-maudios-prod.smil/playlist.m3u8'/>*/}
+      {/*<ShakaPlayer ref={ref} src='https://storage.googleapis.com/shaka-demo-assets/bbb-dark-truths-hls/hls.m3u8'/>*/}
     </div>
   );
 }
